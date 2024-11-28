bd9823414305ade42e83797772558ed5bacb1fc1
ouclei <lhl9106@stu.ouc.edu.cn>
2024-11-28 17:42:07 +0800

    put them into one file to run

---
+++ run_them.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 16:45:46 2024
+
+@author: lei
+"""
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import forehand_function as ff
+import robo_num_main
+import success_main
+import distribution_main
+
+# 
+if __name__ == "__main__":
+    success_main()
+    robo_num_main()
+    distribution_main()
+    
+    
