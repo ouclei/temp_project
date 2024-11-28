e487f2b8cc4cd54305452caf3358ad82a7bc8878
ouclei <lhl9106@stu.ouc.edu.cn>
2024-11-28 17:56:49 +0800

    Update run_them.py to correctly call main functions

--- run_them.py
+++ run_them.py
@@ -9,14 +9,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import forehand_function as ff
-import robo_num_main
-import success_main
-import distribution_main
+import robo_num_main as robo
+import success_main as su
+import distribution_main as dis
 
 # 
 if __name__ == "__main__":
-    success_main()
-    robo_num_main()
-    distribution_main()
+    su.success_main()
+    robo.robo_num_main()
+    dis.distribution_main()
     
     
