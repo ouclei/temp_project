d26c4c8c7c28af56cacc235357c7c4df117b297a
ouclei <lhl9106@stu.ouc.edu.cn>
2024-11-28 20:09:10 +0800

    增加了三个调试函数，分别运行

---
+++ run_success.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 20:06:02 2024
+
+@author: lei
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 16:45:46 2024
+
+@author: lei
+"""
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import forehand_function as ff
+import robo_num_main as robo
+import success_main as su
+import distribution_main as dis
+import time 
+
+# 
+if __name__ == "__main__":
+    time_start = time.time()
+    print(f"now start with time = {time_start - time_start}s ")
+    
+    su.success_main()
+    time_elaps_fir = time.time() - time_start
+    print(f'fir project \'s done with time = {time_elaps_fir}s ')
